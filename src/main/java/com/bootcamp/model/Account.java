96475cbf09ebd0ffb80001b872b6c0fc444cd48e
mrjupe <mr.jupe23@gmail.com>
2018-12-24 10:33:54 +0700

    Adding Account model

---
+++ src/main/java/com/bootcamp/model/Account.java
@@ -0,0 +1,47 @@
+package com.bootcamp.model;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Account {
+
+	@Id
+	private int accountnumber;
+	@Column
+	private Date opendate;
+	@Column
+	private Float balance;
+	@ManyToOne
+	@JoinColumn(name="customerid")
+	private Customer customer;
+	public int getAccountnumber() {
+		return accountnumber;
+	}
+	public void setAccountnumber(int accountnumber) {
+		this.accountnumber = accountnumber;
+	}
+	public Date getOpendate() {
+		return opendate;
+	}
+	public void setOpendate(Date opendate) {
+		this.opendate = opendate;
+	}
+	public Float getBalance() {
+		return balance;
+	}
+	public void setBalance(Float balance) {
+		this.balance = balance;
+	}
+	public Customer getCustomer() {
+		return customer;
+	}
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+}
