cbbd6213ce685ad93df051e44671469b8444b288
mrjupe <mr.jupe23@gmail.com>
2018-12-24 10:28:02 +0700

    Adding http method get, post, put and delete on Customer controller

--- src/main/java/com/bootcamp/CustomerController.java
+++ src/main/java/com/bootcamp/CustomerController.java
@@ -3,7 +3,12 @@ package com.bootcamp;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -45,5 +50,27 @@ public class CustomerController {
 			e.printStackTrace();
 		}
 		return list;
+	}
+	
+	@PostMapping("")
+	public Customer insert(@RequestBody Customer customer)throws Exception{
+		customerDao.save(customer);
+		return customer;
+	}
+	
+	@PutMapping("")
+	public Customer update(@RequestBody Customer customer) throws Exception{
+		Customer update = customerDao.save(customer);
+		return update;
+	}
+	
+	@DeleteMapping("/{id}")
+	public String delete(@PathVariable("id") Customer customer) throws Exception{
+		try {
+			customerDao.delete(customer);
+			return "Delete success";
+		} catch (Exception e) {
+			return "Delete failed";
+		}
 	}
 }
